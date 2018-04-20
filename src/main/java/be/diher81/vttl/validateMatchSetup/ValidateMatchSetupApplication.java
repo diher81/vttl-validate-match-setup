04da0f756cb1361067300d710e72739f24773a0a
Hermans Dirk (100) <dirk.hermans@cm.be>
2018-04-20 21:39:41 +0200

    first commit

---
+++ src/main/java/be/diher81/vttl/validateMatchSetup/ValidateMatchSetupApplication.java
@@ -0,0 +1,12 @@
+package be.diher81.vttl.validateMatchSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateMatchSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateMatchSetupApplication.class, args);
+	}
+}
