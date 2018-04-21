6c8c00abb802526633647c7c600d90c92b9c87c1
Hermans Dirk (100) <dirk.hermans@cm.be>
2018-04-21 16:48:44 +0200

    maven backbone ok. Runs on localhost.

--- war/src/main/java/be/diher81/vttl/war/ValidateMatchSetupApplication.java
+++ war/src/main/java/be/diher81/vttl/war/ValidateMatchSetupApplication.java
@@ -1,4 +1,4 @@
-package be.diher81.vttl.validateMatchSetup;
+package be.diher81.vttl.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
